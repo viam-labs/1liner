e66bf5a10fad5f536a781c77995cd18ec353c699
Abe Winter <abe-winter@users.noreply.github.com>
2024-03-15 14:47:07 -0400

    working version

--- mymodel.py
+++ mymodel.py
@@ -4,5 +4,5 @@ from viam.components.sensor import Sensor
 class Model(Sensor):
     MODEL = "viam-labs:lowcode:sensor"
 
-    def get_readings(self, extra: dict | None) -> dict:
+    async def get_readings(self, extra: dict | None, **kwargs) -> dict:
         return {"success?": True}
